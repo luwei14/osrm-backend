256a4b9222e3dee1f52d0644f0e78952553b76f9
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2016-01-20 20:55:12 +0100

    Really include all dependencies for static linking

--- example/cmake/FindLibOSRM.cmake
+++ example/cmake/FindLibOSRM.cmake
@@ -18,22 +18,46 @@ find_path(LibOSRM_INCLUDE_DIR osrm/osrm.hpp
   /usr
   /opt/local
   /opt)
-
-find_library(LibOSRM_LIBRARY Names osrm libosrm
-  PATH_SUFFIXES osrm lib/osrm lib
-  HINTS ${PC_LibOSRM_LIBDIR} ${PC_LibOSRM_LIBRARY_DIRS}
-  ~/Library/Frameworks
-  /Library/Frameworks
-  /usr/local
-  /usr
-  /opt/local
-  /opt)
-
-set(LibOSRM_LIBRARIES ${LibOSRM_LIBRARY})
 set(LibOSRM_INCLUDE_DIRS ${LibOSRM_INCLUDE_DIR})
+
+if ("${PC_LibOSRM_STATIC_LIBRARIES}" STREQUAL "")
+  find_library(LibOSRM_STATIC_LIBRARY Names osrm.lib libosrm.a
+    PATH_SUFFIXES osrm lib/osrm lib
+    HINTS ${PC_LibOSRM_LIBDIR} ${PC_LibOSRM_LIBRARY_DIRS}
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /opt/local
+    /opt)
+  set(LibOSRM_STATIC_LIBRARIES ${LibOSRM_STATIC_LIBRARY})
+else()
+  set(LibOSRM_STATIC_LIBRARIES ${PC_LibOSRM_STATIC_LIBRARIES})
+endif()
+
+if ("${PC_LibOSRM_LIBRARIES}" STREQUAL "")
+  find_library(LibOSRM_DYNAMIC_LIBRARY Names osrm.dynlib libosrm.so
+    PATH_SUFFIXES osrm lib/osrm lib
+    HINTS ${PC_LibOSRM_LIBDIR} ${PC_LibOSRM_LIBRARY_DIRS}
+    ~/Library/Frameworks
+    /Library/Frameworks
+    /usr/local
+    /usr
+    /opt/local
+    /opt)
+  set(LibOSRM_DYNAMIC_LIBRARIES ${LibOSRM_DYNAMIC_LIBRARY})
+else()
+  set(LibOSRM_DYNAMIC_LIBRARIES ${PC_LibOSRM_LIBRARIES})
+endif()
+
+if ("${LibOSRM_STATIC_LIBRARIES}" STREQUAL "")
+  set(LibOSRM_LIBRARIES ${LibOSRM_DYNAMIC_LIBRARIES})
+else()
+  set(LibOSRM_LIBRARIES ${LibOSRM_STATIC_LIBRARIES})
+endif()
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBOSRM_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(LibOSRM DEFAULT_MSG
-                                LibOSRM_LIBRARY LibOSRM_INCLUDE_DIR)
+                                LibOSRM_LIBRARIES LibOSRM_INCLUDE_DIR)
